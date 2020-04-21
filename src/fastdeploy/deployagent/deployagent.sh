461fb54612169e78555061fa56377704007bc5ab
zhongkaizhu <zhongkaizhu@tencent.com>
2020-04-21 11:46:41 +0800

    [29.0.6] remove old code

--- src/fastdeploy/deployagent/deployagent.sh
+++
@@ -1,7 +0,0 @@
-# Script to start "deployagent" on the device, which has a very rudimentary
-# shell.
-#
-base=/data/local/tmp
-export CLASSPATH=$base/deployagent.jar
-exec app_process $base com.android.fastdeploy.DeployAgent "$@"
-
